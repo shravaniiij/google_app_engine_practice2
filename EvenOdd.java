3dd153af70815838aee99d047909614ac52fa0ce
shravaniiij <shravani.jaigude825@gmail.com>
2026-05-14 00:18:42 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+
+    public static void main() {
+
+        Integer num = 5;
+
+        if(num % 2 == 0)
+            System.debug('Even');
+        else
+            System.debug('Odd');
+    }
+}
